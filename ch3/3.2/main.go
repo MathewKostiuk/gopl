2ba87637dfeb735812e28b080de364e1945cc291
MathewKostiuk <mathewkostiuk@gmail.com>
2021-08-25 21:15:00 -0700

    Connected query params to program

--- ch3/3.2/main.go
+++ ch3/3.2/main.go
@@ -7,51 +7,73 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrang..+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6         // angle of x, y axes (=30°)
-	peaks         = "#ff0000"           // Colour of the peaks (red)
-	valleys       = "#0000ff"           // Colour of the valleys (blue)
+	cells   = 100         // number of grid cells
+	xyrange = 30.0        // axis ranges (-xyrang..+xyrange)
+	angle   = math.Pi / 6 // angle of x, y axes (=30°)
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+var width, height int
+var xyscale, zscale = 0.0, 0.0
 
 func main() {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "image/svg+xml")
-		writeSVG(w)
+		width, height, xyscale, zscale, p, v := parseQuery(r)
+		writeSVG(w, width, height, xyscale, zscale, p, v)
 	}
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 	return
 }
 
-func writeSVG(out io.Writer) {
+func parseQuery(r *http.Request) (float64, float64, float64, float64, string, string) {
+	w := r.URL.Query()["width"]
+	h := r.URL.Query()["height"]
+	p := r.URL.Query()["peaks"]
+	v := r.URL.Query()["valleys"]
+	width, err := strconv.Atoi(w[0])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	height, err := strconv.Atoi(h[0])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	xyscale := width / 2 / xyrange
+	zscale := float64(height) * 0.4
+
+	return float64(width), float64(height), float64(xyscale), zscale, p[0], v[0]
+}
+
+func writeSVG(out io.Writer, width, height, xyscale, zscale float64, p, v string) {
 	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
+		"width='%v' height='%v'>", width, height)
 
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, neg := corner(i+1, j)
+			ax, ay, neg := corner(i+1, j, width, height, xyscale, zscale)
 			if math.IsNaN(ax) {
 				continue
 			}
-			bx, by, neg := corner(i, j)
+			bx, by, neg := corner(i, j, width, height, xyscale, zscale)
 			if math.IsNaN(bx) {
 				continue
 			}
-			cx, cy, neg := corner(i, j+1)
+			cx, cy, neg := corner(i, j+1, width, height, xyscale, zscale)
 			if math.IsNaN(cx) {
 				continue
 			}
-			dx, dy, neg := corner(i+1, j+1)
+			dx, dy, neg := corner(i+1, j+1, width, height, xyscale, zscale)
 			if math.IsNaN(dx) {
 				continue
 			}
@@ -59,9 +81,9 @@ func writeSVG(out io.Writer) {
 			var fill string
 
 			if neg {
-				fill = valleys
+				fill = v
 			} else {
-				fill = peaks
+				fill = p
 			}
 
 			fmt.Fprintf(out, "<polygon fill='%s' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
@@ -71,7 +93,7 @@ func writeSVG(out io.Writer) {
 	fmt.Fprintf(out, "</svg>")
 }
 
-func corner(i, j int) (float64, float64, bool) {
+func corner(i, j int, width, height, xyscale, zscale float64) (float64, float64, bool) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
