90a17a2034841bf3790c334d0a0719515cb9e2e2
MathewKostiuk <mathewkostiuk@gmail.com>
2021-08-31 21:38:23 -0700

    Complete exercise using slices

--- ch4/4.4/main.go
+++ ch4/4.4/main.go
@@ -6,11 +6,14 @@ import (
 
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
-	p := rotate(a[:], 2)
-	s := rotate(a[:], -2)
+	p := rotate(a[:], 4)
+	s := rotate(a[:], 2)
 	fmt.Println(a)
 	fmt.Println(p)
 	fmt.Println(s)
+
+	r := rotateSlice(a[:], 4)
+	fmt.Println(r)
 }
 
 // Rotate in a single pass
@@ -30,5 +33,12 @@ func rotate(s []int, n int) []int {
 		r[i] = s[i+n]
 	}
 
+	return r
+}
+
+func rotateSlice(s []int, n int) []int {
+	s1 := s[n:]
+	s2 := s[:n]
+	r := append(s1, s2...)
 	return r
 }
