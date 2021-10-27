5cf14b193ec076387a03c1cad55d6abf43e487af
MathewKostiuk <mathewkostiuk@gmail.com>
2021-10-26 20:10:39 -0700

    Part 1 of exercise 8.1

---
+++ ch8/clock2/clock2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+var port = flag.String("port", "8000", "Used to set what port to listen for connections on. Defaults to 8000")
+
+func main() {
+	flag.Parse()
+	listener, err := net.Listen("tcp", "localhost:"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05 MST\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
