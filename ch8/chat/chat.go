904b85978fb4526aec5b90540814ae90c726fa44
MathewKostiuk <mathewkostiuk@gmail.com>
2021-11-06 16:37:52 -0700

    Complete exercise 8.12

---
+++ ch8/chat/chat.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+type client struct {
+	name string
+	ch   chan<- string // an outgoing message channel
+}
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string)
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming messages to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli.ch <- msg
+			}
+		case nc := <-entering:
+			clients[nc] = true
+			for cli := range clients {
+				if cli.name != nc.name {
+					nc.ch <- cli.name + " is also here"
+				}
+			}
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+
+	name := conn.RemoteAddr().String()
+	client := client{name, ch}
+	ch <- "You are " + name
+	messages <- name + " has arrived"
+	entering <- client
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- name + ": " + input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	leaving <- client
+	messages <- name + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
